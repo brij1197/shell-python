ff45ba831019c42554d8aa73519de3156e588bf3
Brij Bhatia <brijbhatia997@gmail.com>
2025-03-14 11:34:21 -0700

    'Exit_Builtin'

--- app/main.py
+++ app/main.py
@@ -8,6 +8,17 @@ def main():
     # Wait for user input
     command=input()
     
+    if command.startswith('exit'):
+        parts = command.split()
+        if len(parts) > 1:
+            try:
+                exit_code = int(parts[1])
+                sys.exit(exit_code)
+            except ValueError:
+                print(f'exit: {parts[1]}: numeric argument required')
+        else:
+            sys.exit(0)
+      
     print(f'{command}: command not found')
     
     main()
