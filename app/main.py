8f24eab5e3dc18bfa4d23501228475250497845a
Brij B <brijbhatia997@gmail.com>
2025-03-14 12:31:54 -0700

    Added Echo

--- app/main.py
+++ app/main.py
@@ -6,25 +6,24 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    command=input()
+    command=input().strip()
     
-    match command:
-        case [command.startswith('exit')]:
-            parts = command.split()
-            if len(parts) > 1:
-                try:
-                    exit_code = int(parts[1])
-                    sys.exit(exit_code)
-                except ValueError:
-                    print(f'exit: {parts[1]}: numeric argument required')
-            else:
-                sys.exit(0)
+    if command.startswith('exit'):
+        parts = command.split()
+        if len(parts) > 1:
+            try:
+                exit_code = int(parts[1])
+                sys.exit(exit_code)
+            except ValueError:
+                print(f'exit: {parts[1]}: numeric argument required')
+        else:
+            sys.exit(0)
         
-        case [command.startswith('echo')]:
-            print(command[5:])
+    elif command.startswith('echo'):
+        print(command[5:])
         
-        case _:
-            print(f'{command}: command not found')
+    else:
+        print(f'{command}: command not found')
         
     main()
 
