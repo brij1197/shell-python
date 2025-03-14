301bb3fa970e773e068bfe013bcb6a41a28b7a3a
Brij B <brijbhatia997@gmail.com>
2025-03-14 16:36:32 -0700

    Added_External_Commands

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import shutil
+import subprocess
 
 SHELL_COMMANDS= ['exit','echo','type']
 
@@ -30,30 +31,59 @@ def shell_type(*args):
             print(f'{parts[1]}: not found')
     else:
         print('type: missing argument')
+        
+def execute_external_command(command):
+    parts=command.split()
+    path=shutil.which(parts[0])
+    
+    if path:
+        try:
+            result=subprocess.run(
+                parts,
+                capture_output=True,
+                text=True,
+                check=True
+            )
+            
+            if result.stdout:
+                print(result.stdout.strip())
+            if result.stderr:
+                print(result.stderr.rstrip(), file=sys.stderr)
+            if result.returncode!=0:
+                sys.exit(result.returncode)
+        except subprocess.SubprocessError as e:
+            print(f'Error executing {parts[0]}:str{e}',file=sys.stderr)
+            return False
+        return True
+    return False
 
 def main():
-  
-    sys.stdout.write("$ ")
+    try:
+        sys.stdout.write("$ ")
 
-    command=input().strip()
-    
-    match command:
-        case command if command.startswith('exit'):
-            parts = command.split()
-            shell_exit(parts)
+        command=input().strip()
         
-        case command if command.startswith('echo'):
-            shell_echo(command)
-        
-        case command if command.startswith('type'):
-            parts = command.split()
-            shell_type(parts)
-        
-        case _:
-            print(f'{command}: command not found')
-        
-    main()
-
+        match command:
+            case command if command.startswith('exit'):
+                parts = command.split()
+                shell_exit(parts)
+            
+            case command if command.startswith('echo'):
+                shell_echo(command)
+            
+            case command if command.startswith('type'):
+                parts = command.split()
+                shell_type(parts)
+            
+            case _:
+                if not execute_external_command(command):
+                    print(f'{command}: command not found')
+    except EOFError:
+        sys.exit(0)
+    except KeyboardInterrupt:
+        print()
+            
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
